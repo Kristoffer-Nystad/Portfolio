0b0f9fa17cfd713d25a8cc979e9fd0a559d1a4f4
Kristoffer Nystad <72980047+Kristoffer-Nystad@users.noreply.github.com>
2022-12-30 22:31:38 +0100

    made app responsive and startet omMeg
    Jeg må finne et annet design for om meg seksjonen da planlagt design ikke vil funke på en estetisk måte.

--- khn_portfolio/lib/views/home/home_view.dart
+++ khn_portfolio/lib/views/home/home_view.dart
@@ -7,24 +7,75 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: const Color.fromRGBO(25, 30, 43, 1),
-        body: Column(
-          children: [
-            Center(
-              child: Container(
-                padding: const EdgeInsets.fromLTRB(50, 100, 50, 0),
-                child: Image.asset('images/logoWhite.png'),
-              ),
-            ),
-            const Center(
-              child: Text(
-                'Fullstack utvikler og webdesigner.',
-                style: TextStyle(
-                    fontFamily: 'Roboto',
-                    fontSize: 40,
-                    color: Color.fromRGBO(228, 233, 252, 1)),
-              ),
-            ),
-          ],
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            if (constraints.maxWidth > 600) {
+              return const _HomeViewDesktop();
+            } else {
+              return const _HomeViewMobile();
+            }
+          },
         ));
+  }
+}
+
+class _HomeViewDesktop extends StatelessWidget {
+  const _HomeViewDesktop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+            child: Image.asset('images/logoWhite.png'),
+          ),
+        ),
+        const Center(
+          child: Text(
+            'Fullstack utvikler og webdesigner.',
+            style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 40,
+                color: Color.fromRGBO(228, 233, 252, 1)),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _HomeViewMobile extends StatefulWidget {
+  const _HomeViewMobile({super.key});
+
+  @override
+  State<_HomeViewMobile> createState() => __HomeViewMobileState();
+}
+
+class __HomeViewMobileState extends State<_HomeViewMobile> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Container(
+            padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
+            child: Image.asset('images/logoWhite.png'),
+          ),
+        ),
+        const Center(
+          child: Text(
+            'Fullstack utvikler og webdesigner.',
+            style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 21,
+                color: Color.fromRGBO(228, 233, 252, 1)),
+          ),
+        ),
+      ],
+    );
   }
 }
--- khn_portfolio/lib/views/omMeg/omMeg_view.dart
+++ khn_portfolio/lib/views/omMeg/omMeg_view.dart
@@ -6,8 +6,62 @@ class OmMegView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(25, 30, 43, 100.00),
-      body: Column(children: <Widget>[]),
+        backgroundColor: const Color.fromRGBO(25, 30, 43, 1),
+        body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+            if (constraints.maxWidth > 600) {
+              return const _OmMegViewDesktop();
+            } else {
+              return const _OmMegViewMobile();
+            }
+          },
+        ));
+  }
+}
+
+class _OmMegViewDesktop extends StatelessWidget {
+  const _OmMegViewDesktop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text(
+        'Hei, jeg er en fullstack utvikler og webdesigner med en \nbachelor grad i IT og Informasjonssystemer fra USN og \n kommer fra en liten plass i Nord-Norge som heter Hatteng \n i Storfjord kommune. \n\nJeg har blitt fortalt at jeg er en god lytter og flink å snakke \n for meg og en god lagkamerat.\n\nJeg har mye forskjellig jobberfaringer fra mange \nforskjellige bransjer slik som helsevesenet, transport og \nturisme under belte. Noe alle disse erfaringene fra disse \nbransjene har lært meg er at arbeidsvilje og vilje til lære er \nde beste egenskapene noen kan ha som arbeidstaker, noe \njeg har da har bøttelast av.\n\nJeg har også mange hobbyer, slik som digital \nillustrasjoner, fotografering, friluftsliv og spille D&D \n(dungeons and dragons)',
+        style: TextStyle(
+            fontFamily: 'Roboto',
+            fontSize: 20,
+            color: Color.fromRGBO(228, 233, 252, 1)),
+      ),
+    );
+  }
+}
+
+class _OmMegViewMobile extends StatelessWidget {
+  const _OmMegViewMobile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Center(
+          child: Image.asset(
+            'images/profilbilde.png',
+            width: 300,
+          ),
+        ),
+        Center(
+            child: Container(
+          color: Color.fromRGBO(47, 90, 200, 1),
+          padding: const EdgeInsets.all(25),
+          child: const Text(
+            'Hei, jeg er en fullstack utvikler og webdesigner med en \nbachelor grad i IT og Informasjonssystemer fra USN og \n kommer fra en liten plass i Nord-Norge som heter Hatteng \n i Storfjord kommune. \n\nJeg har blitt fortalt at jeg er en god lytter og flink å snakke \n for meg og en god lagkamerat.\n\nJeg har mye forskjellig jobberfaringer fra mange \nforskjellige bransjer slik som helsevesenet, transport og \nturisme under belte. Noe alle disse erfaringene fra disse \nbransjene har lært meg er at arbeidsvilje og vilje til lære er \nde beste egenskapene noen kan ha som arbeidstaker, noe \njeg har da har bøttelast av.\n\nJeg har også mange hobbyer, slik som digital \nillustrasjoner, fotografering, friluftsliv og spille D&D \n(dungeons and dragons)',
+            style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 12,
+                color: Color.fromRGBO(228, 233, 252, 1)),
+          ),
+        )),
+      ],
     );
   }
 }
